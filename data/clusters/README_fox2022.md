63dab494a2e87986c0217e78da1dd29c555f4b1c
Leonard Speiser <leonard@horizon3.net>
2025-11-30 15:06:26 -0800

    Add Fox+2022 cluster lensing database (75 clusters, 111 models)
    Source: CDS J/ApJ/928/87 - Strong lensing masses, Einstein radii, M500/M200 from HFF/RELICS/SGAS surveys.

---
+++ data/clusters/README_fox2022.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ed151c90a4ef04dfb208ed6f82a7cfbbdb41cd6bb6d03e783251e3404691b75
+size 4546
---
+++ data/clusters/parse_fox2022.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e30f768eef4d4d5eb6a1eb39c1866109716893e828c55578fbbdc8c8cbb59c30
+size 8827
